08254ccaac9a249b3567aaec77e7675895743d2f
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-06-07 12:09:09 -0400

    Update postfix.sh

--- postfix.sh
+++ postfix.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
+!/bin/bash
 ### In postfix.sh (make sure this file is chmod +x):
 # `chpst -u root` runs the given command as the user `xxxxx`.
 # If you omit that part, the command will be run as root.
 
-exec chpst -u postfix postfix start -d -c /etc/postfix 2>&1
+ daemon_directory=/usr/lib/sbin/postfix
+ command_directory=/usr/sbin
+ config_directory=/etc/postfix
+ queue_directory=/var/spool/postfix
+ mail_owner=postfix
+ setgid_group=postdrop
+
+
+exec /usr/lib/postfix/sbin/master  2>&1
