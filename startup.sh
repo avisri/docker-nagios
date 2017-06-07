3896f5cdc1c850857f49b79aed28243670b90399
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-06-07 13:19:29 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,10 +5,12 @@ set -e
 
 if [ -f /etc/configured ]; then
         echo 'already configured'
+        postfix start
 else
       #code that need to run only one time ....
         
         #needed for fix problem with ubuntu and cron
         update-locale 
+        postfix start
         date > /etc/configured
 fi
