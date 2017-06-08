ad4db9fb499179a03b9239094f454e934d0bf7c3
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2017-06-08 10:42:41 -0400

    Create startup.sh

--- startup.sh
+++ startup.sh
@@ -12,8 +12,9 @@ else
         update-locale 
         echo 'root:  me@example.com' >>/etc/aliases
         newaliases
-        # cat /etc/hosts | grep $HOSTNAME| awk -F. '{print $1 "." $2 ".0.0/16"}'
-        # cat /etc/hosts | grep $HOSTNAME| awk -F\  '{print $1}'
+        #add container Network Docker0 and container ip to postfix configuration , it will fail is custom container network
+        sed -i '/inet_interfaces/s/$/ '"$(cat /etc/hosts | grep $HOSTNAME| awk -F\  '{print $1}')"'/' /etc/postfix/main.cf
+        sed -i '/mynetworks/s/$/ '"$(cat /etc/hosts | grep $HOSTNAME| awk -F. '{print $1 "." $2 ".0.0\/16"}')"'/' /etc/postfix/main.cf
         postfix start
         date > /etc/configured
 fi
